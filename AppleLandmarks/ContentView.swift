6cf3cf428b9370694e8411a4135b54eaaacd409b
Quentin Genevois <quentin.g@appsolute.fr>
2019-10-16 09:38:46 +0200

    Initial Commit

---
+++ AppleLandmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleLandmarks
+//
+//  Created by Quentin Genevois on 16/10/2019.
+//  Copyright © 2019 Quentin Genevois. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
