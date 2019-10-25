3a46527c6d3128a200d78ae24cb46db6ca32a3d2
Quentin Genevois <quentin.g@appsolute.fr>
2019-10-25 11:14:53 +0200

    Squashed commit of the following:
    commit 4b69e0c25e770489267c4957f9f5c893a082743b
    Author: Quentin Genevois <quentin.g@appsolute.fr>
    Date:   Fri Oct 25 11:14:38 2019 +0200
    
        Created home view

--- AppleLandmarks/LandmarkList.swift
+++ AppleLandmarks/LandmarkList.swift
@@ -12,31 +12,30 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
 
     var body: some View {
-        NavigationView {
-            List {
-                Toggle(isOn: $userData.showFavoritesOnly) {
-                    Text("Favorite Only")
-                }
+        List {
+            Toggle(isOn: $userData.showFavoritesOnly) {
+                Text("Favorite Only")
+            }
 
-                ForEach(userData.landmarks) { landmark in
-                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                            LandmarkRow(landmark: landmark)
-                        }
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
                     }
                 }
             }
-            .navigationBarTitle(Text("Landmarks"))
         }
+        .navigationBarTitle(Text("Landmarks"))
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
+            NavigationView {
+                LandmarkList()
+                    .environmentObject(UserData())
+            }
         }
     }
 }
--- AppleLandmarks/SceneDelegate.swift
+++ AppleLandmarks/SceneDelegate.swift
@@ -22,8 +22,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView:
-                LandmarkList()
+            window.rootViewController = UIHostingController(
+                rootView: CategoryHome()
                     .environmentObject(UserData())
             )
             self.window = window
