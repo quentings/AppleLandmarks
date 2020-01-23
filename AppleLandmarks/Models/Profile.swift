21d4ff11ac1df36b86d214cdb713dafd36a8a644
Quentin Genevois <quentin.g@appsolute.fr>
2020-01-23 09:23:35 +0100

    Changed username

--- AppleLandmarks/Models/Profile.swift
+++ AppleLandmarks/Models/Profile.swift
@@ -12,7 +12,7 @@ struct Profile {
     var seasonalPhoto: Season
     var goalDate: Date
     
-    static let `default` = Self(username: "g_kumar", prefersNotifications: true, seasonalPhoto: .winter)
+    static let `default` = Self(username: "QuentinG", prefersNotifications: true, seasonalPhoto: .winter)
     
     init(username: String, prefersNotifications: Bool = true, seasonalPhoto: Season = .winter) {
         self.username = username
