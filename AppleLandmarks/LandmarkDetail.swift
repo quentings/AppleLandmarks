444ae3bf1694d66004d3e126f5119a12e3afb360
Quentin Genevois <quentin.g@appsolute.fr>
2020-01-29 09:48:18 +0100

    Fixed details view

--- AppleLandmarks/LandmarkDetail.swift
+++ AppleLandmarks/LandmarkDetail.swift
@@ -17,49 +17,46 @@ struct LandmarkDetail: View {
     }
 
     var body: some View {
-        VStack {
+        VStack  {
             MapView(coordinate: landmark.locationCoordinate)
+                .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
 
             CircleImage(image: landmark.image)
-                .offset(y: -130)
+                .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
 
-            VStack {
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text(landmark.name)
-                            .font(.title)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
 
-
-                        Button(action: {
-                            self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
-                        }) {
-                            if self.userData.landmarks[self.landmarkIndex].isFavorite {
-                                Image(systemName: "star.fill")
-                                    .foregroundColor(Color.yellow)
-                            } else {
-                                Image(systemName: "star")
-                                    .foregroundColor(Color.gray)
-                            }
+                    Button(action: {
+                        self.userData.landmarks[self.landmarkIndex]
+                            .isFavorite.toggle()
+                    }) {
+                        if self.userData.landmarks[self.landmarkIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(Color.gray)
                         }
                     }
-
-                    HStack {
-                        Text(landmark.park)
-                            .font(.subheadline)
-                        Spacer()
-                        Text(landmark.state)
-                            .font(.subheadline)
-                    }
                 }
-                .padding()
 
-                Spacer()
+                HStack(alignment: .top) {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
             }
+            .padding()
+
+            Spacer()
         }
-        .navigationBarTitle(Text(landmark.name),
-                            displayMode: .inline)
     }
 }
 
